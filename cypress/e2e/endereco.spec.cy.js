bfc40c896dea89691a3aa1eedafa2ef9a33dde90
Kevin <kevindff@hotmail.com>
2023-09-14 23:26:54 -0300

    Massa de dados

--- cypress/e2e/endereco.spec.cy.js
+++ cypress/e2e/endereco.spec.cy.js
@@ -1,5 +1,6 @@
 /// <reference types = "cypress"/>
-
+import enderecoPage from "../support/page-objects/endereco.page";
+const dadosEndereco = require('../fixtures/endereco.json')
 describe(' Funcionalidade Endereços - Faturamento e Entrega', () => {
     
     beforeEach(() => {
@@ -11,6 +12,25 @@ describe(' Funcionalidade Endereços - Faturamento e Entrega', () => {
 
     });
     it('Deve fazer cadastro de faturamente com sucesso', () => {
-        
+        enderecoPage.editarEnderecoFaturamento('Flavio', 'Fonseca', '4code', 'Brasil', 'Av. Brasil', '3100', 'São paulo', 'São Paulo', '76380000', '62985635422', 'email@email.com')
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
+    });
+
+    it.only('Deve fazer cadastro de faturamente com sucesso - Usando arquivo de daods', () => {
+        enderecoPage.editarEnderecoFaturamento(
+            dadosEndereco[1].nome,
+            dadosEndereco[1].sobrenome,
+            dadosEndereco[1].empresa,
+            dadosEndereco[1].endereco,
+            dadosEndereco[1].numero,
+            dadosEndereco[1].pais,
+            dadosEndereco[1].cidade,
+            dadosEndereco[1].estado,
+            dadosEndereco[1].cep,
+            dadosEndereco[1].telefone,
+            dadosEndereco[1].email
+
+            )
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
     });
 });
